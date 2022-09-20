492b48da787426a9e87fa40ba6d81a5a1eea1ac8
artem-chigin <chiartftc@gmail.com>
2022-09-20 09:48:20 +0400

    added connection to the database.

--- program.py
+++ program.py
@@ -1,32 +1,59 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 import requests
 
-
 import sys
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+db = SQLAlchemy(app)
 
-add_flag = False
+
+class Weather(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city_name = db.Column(db.String(50), unique=True)
+    # temperature = db.Column(db.Integer)
+    # weather_state = db.Column(db.String(50))
+
+
+db_path = os.path.join("/", "weather.db")
+if not os.access(db_path, os.F_OK):
+    db.create_all()
+
+
+db_is_empty = False
+
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template('index.html')
+        from_db = Weather.query.all()
+
+        result = []
+        for entry in from_db:
+            city_name = entry.city_name
+            api_id = "24034c2fc253da6475cd74bc0b96cf5a"
+            api_link = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={api_id}"
+            dict_with_info = requests.get(api_link).json()
+
+            city = dict_with_info["name"]
+            temperature = int(dict_with_info["main"]["temp"]) - 273
+            state = dict_with_info["weather"][0]["main"]
+            result.append({"city_name": city, "temperature": temperature, "state": state})
+
+        return render_template('index.html', info=result, x=from_db)
     elif request.method == "POST":
         city_name = request.form["city_name"]
-        api_id = "24034c2fc253da6475cd74bc0b96cf5a"
-        api_link = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={api_id}"
 
-        dict_with_weather_info = requests.get(api_link).json()
-        add_flag = True
-        city = dict_with_weather_info["name"]
-        temperature = int(dict_with_weather_info["main"]["temp"]) - 273
-        state = dict_with_weather_info["weather"][0]["main"]
-        return render_template("index.html",  add_flag=add_flag, state=state, city=city, temperature=temperature)
+        new_entry = Weather(city_name=city_name)
+        db.session.add(new_entry)
+        db.session.commit()
+        # from_db = Weather.query.all()
+
+        return redirect("/")
 
 
 
